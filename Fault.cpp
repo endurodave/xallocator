0e6a2769535837d14304db70bdd0e1373efc62f8
David Lafreniere <dlafreniere@cytori.com>
2017-10-28 08:04:06 -0700

    First commit.

---
+++ Fault.cpp
@@ -0,0 +1,14 @@
+#include "Fault.h"
+#include "DataTypes.h"
+#include <assert.h>
+
+//----------------------------------------------------------------------------
+// FaultHandler
+//----------------------------------------------------------------------------
+void FaultHandler(const char* file, unsigned short line)
+{
+	// If you hit this line, it means one of the ASSERT macros failed.
+    DebugBreak();	
+
+	assert(0);
+}
